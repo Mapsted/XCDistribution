1bc4e7362e156aeb47a4557e79f9c80b85f6160f
Daniel Teklegiorgis <d.teklegiorgis@mapsted.com>
2022-05-02 15:02:06 -0400

    Added MapstedAR xcframework
    Added XCFramework

---
+++ MapstedAR.xcframework/ios-arm64_x86_64-simulator/MapstedAR.framework/Headers/MapstedAR.h
@@ -0,0 +1,18 @@
+//
+//  MapstedAR.h
+//  MapstedAR
+//
+//  Created by Daniel on 2022-05-02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapstedAR.
+FOUNDATION_EXPORT double MapstedARVersionNumber;
+
+//! Project version string for MapstedAR.
+FOUNDATION_EXPORT const unsigned char MapstedARVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapstedAR/PublicHeader.h>
+
+
